dfffe48265ac21a46f1b7e83504c8e890988490d
Youbin Park <a44121078@gmail.com>
2023-07-16 13:57:47 +0900

    프로젝트 세팅

---
+++ src/main/java/com/DDakDae/reactAndspring/ReactAndspringApplication.java
@@ -0,0 +1,13 @@
+package com.DDakDae.reactAndspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactAndspringApplication.class, args);
+	}
+
+}
